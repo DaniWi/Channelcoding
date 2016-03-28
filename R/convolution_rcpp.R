0624ea187d7c2f5d824a4bbf22eaae2df512e1e3
MartinNoc <martin.nocker@student.uibk.ac.at>
2016-03-28 18:14:25 +0200

    R documentation roxygen (convolution)

--- R/convolution_rcpp.R
+++ R/convolution_rcpp.R
@@ -1,3 +1,5 @@
+# convolution_rcpp.R
+#
 # interface for convolutional codes
 # provided functions:
 #  - generate nsc coder
@@ -9,7 +11,16 @@
 library(Rcpp);
 sourceCpp('src/convolution.cpp');
 
-# generate nsc coder
+#' generate nsc encoder
+#' 
+#' generates a convolutional encoder for nonsystematic convolutional codes (nsc)
+#' @author Martin Nocker
+#' @param N numer ob output symbols per input symbol
+#' @param M constraint length (memory length of the encoder)
+#' @param generators vector of generator polynoms (one for each output symbol)
+#' @return a convolutional encoder represented as a list containing: 
+#' N, M, 4 matrices: nextState, previousState, output, termination, nsc (flag)
+#' @example generateConvEncoder_nsc(2,2,c(7,5))
 generateConvEncoder_nsc <- function(N, M, generators) {
    
    # nsc requires N generator polynoms
@@ -36,7 +47,16 @@ generateConvEncoder_nsc <- function(N, M, generators) {
    return(convEncoder);
 }
 
-# generate rsc encoder
+#' generate rsc encoder
+#' 
+#' generates a convolutional encoder for recursive systematic codes (rsc)
+#' @author Martin Nocker
+#' @param N numer ob output symbols per input symbol
+#' @param M constraint length (memory length of the encoder)
+#' @param generators vector of generator polynoms (one for each output symbol and one for the recursion)
+#' @return a convolutional encoder represented as a list containing: 
+#' N, M, 4 matrices: nextState, previousState, output, termination, nsc (flag)
+#' @example generateConvEncoder_nsc(2,2,c(1,10,13))
 generateConvEncoder_rsc <- function(N, M, generators) {
    
    # rsc requires N+1 generator polynoms
@@ -63,7 +83,17 @@ generateConvEncoder_rsc <- function(N, M, generators) {
    return(convEncoder_rsc);
 }
 
-# encode of message with convolutional encoder
+#' convolutional encoding of a message
+#' 
+#' \code{conv_encode} produces a convolutional code of a message based on the encoder passed as an argument
+#' @author Martin Nocker
+#' @param message the message to be encoded
+#' @param convEncoder convolutional encoder used for encoding [list]
+#' @param terminate flag if the code should be terminated, default: TRUE
+#' @return the encoded message
+#' @example
+#' encoder <- generateConvEncoder_nsc(2,2,c(7,5))
+#' code <- conv_encode(c(1,0,1), encoder)
 conv_encode <- function(message, convEncoder, terminate = TRUE) {
 
    code <- c_convolutionEncode(message,
@@ -77,6 +107,19 @@ conv_encode <- function(message, convEncoder, terminate = TRUE) {
    return(code);
 }
 
+#' convolutional decoding of a code (soft decision)
+#' 
+#' \code{conv_decode} decodes a codeword that was encoded with the given encoder.
+#' This decoder is a soft-input soft-output decoder
+#' @author Martin Nocker
+#' @param code the code to be decoded
+#' @param convEncoder convolutional encoder used for encoding [list]
+#' @param terminate flag if the code was terminated, default: TRUE
+#' @return the decoded message, list(softOutput, hardOutput)
+#' @example
+#' encoder <- generateConvEncoder_nsc(2,2,c(7,5))
+#' code <- conv_encode(c(1,0,1), encoder)
+#' msg <- conv_decode(code, encoder)
 conv_decode <- function(code, convEncoder, terminate = TRUE) {
    
    output <- c_convolutionDecode(code,
@@ -97,6 +140,19 @@ conv_decode <- function(code, convEncoder, terminate = TRUE) {
    return(output);
 }
 
+#' convolutional decoding of a code (hard decision)
+#' 
+#' \code{conv_decode} decodes a codeword that was encoded with the given encoder.
+#' This decoder is a hard-decision decoder
+#' @author Martin Nocker
+#' @param code the code to be decoded
+#' @param convEncoder convolutional encoder used for encoding [list]
+#' @param terminate flag if the code was terminated, default: TRUE
+#' @return the hard-decoded message vector
+#' @example
+#' encoder <- generateConvEncoder_nsc(2,2,c(7,5))
+#' code <- conv_encode(c(1,0,1), encoder)
+#' msg <- conv_decode_hard(code, encoder)
 conv_decode_hard <- function(code, convEncoder, terminate = TRUE) {
 
    output <- c_convolutionDecode_hard(code,
