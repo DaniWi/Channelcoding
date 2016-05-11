2a26fb41a5a39accf2cacfd5cab0b7141117a4e3
MartinNoc <martin.nocker@student.uibk.ac.at>
2016-05-11 11:54:54 +0200

    Package documentation

--- R/channelcoding.R
+++ R/channelcoding.R
@@ -1,7 +1,11 @@
-#' foo: A package for computating the notorious bar statistic.
+#' Channelcoding Functions for R
 #'
-#' The foo package provides three categories of important functions:
-#' foo, bar and baz.
+#' The channelcoding package provides functionality for three different
+#' channelcodes: block codes, convolutional codes and turbo codes.
+#' Self-defined encoders, encoding, decoding, message distortion as well
+#' as simulations and visualizations are supported.
+#'
+#' @author Martin Nocker, Benedikt Wimmer, Daniel Witsch
 #'
 #'
 #' @docType package
