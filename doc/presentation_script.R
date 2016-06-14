12c10b029b6d69f5360392a135aad97af440902c
MartinNoc <martin.nocker@student.uibk.ac.at>
2016-06-14 15:17:36 +0200

    demo script

--- doc/presentation_script.R
+++ doc/presentation_script.R
@@ -12,20 +12,25 @@ block.coder <- BlockGenerateEncoderBCH(code.length = 15, code.t = 3)
 #
 conv.coder <- ConvGenerateEncoder(N = 2, M = 2, generators = c(7,5))
 
+systematic.coder <- ConvGenerateRscEncoder(2,2,c(5,7))
+
 message <- rep(c(1,0,1,1,1,0,0,0,1,0,1,1,0,1,1,1,0,1,1,0,0,0,1,0,0), times = 4)
 message
 
 perm.vector = TurboGetPermutation(message.length = length(message),
                                   coder.info = conv.coder,
                                   type = "PRIMITIVE",
-                                  args = list(root=2))
+                                  list(root=2),
+                                  visualize = TRUE)
 
 # encode
 block.coded <- BlockEncode(message, block.encoder = block.coder)
+block.coded
 
 conv.coded <- ConvEncode(message, conv.encoder = conv.coder)
+conv.coded
 
-turbo.coded <- TurboEncode(message, permutation.vector = perm.vector, coder.info = conv.coder)
+turbo.coded <- TurboEncode(message, permutation.vector = perm.vector, coder.info = systematic.coder)
 
 # decode
 block.decoded <- BlockDecode(block.coded, block.encoder = block.coder)
@@ -33,7 +38,7 @@ block.decoded <- BlockDecode(block.coded, block.encoder = block.coder)
 conv.decoded <- ConvDecodeSoft(conv.coded, conv.encoder = conv.coder)$output.hard
 
 turbo.decoded <- TurboDecode(turbo.coded, permutation.vector = perm.vector,
-                             iterations = 3, coder.info = conv.coder)$output.hard
+                             iterations = 3, coder.info = systematic.coder)$output.hard
 
 # message = decoded ???
 message - block.decoded
@@ -41,29 +46,26 @@ message - conv.decoded
 message - turbo.decoded
 
 # code with noise
-block.noisy <- ApplyNoise(block.coded, SNR.db = 1, binary = TRUE)
-block.messy <- ApplyNoise(block.coded, SNR.db = 0.1, binary = TRUE)
+Block.noisy <- ApplyNoise(block.coded, SNR.db = 2, binary = TRUE)
+Block.messy <- ApplyNoise(block.coded, SNR.db = 0.1, binary = TRUE)
 
-conv.noisy <- ApplyNoise(conv.coded, SNR.db = 1)
-conv.messy <- ApplyNoise(conv.coded, SNR.db = 0.1)
+Conv.noisy <- ApplyNoise(conv.coded, SNR.db = 2)
+Conv.messy <- ApplyNoise(conv.coded, SNR.db = 0.1)
 
-turbo.noisy <- ApplyNoise(turbo.coded, SNR.db = 1)
-turbo.messy <- ApplyNoise(turbo.coded, SNR.db = 0.1)
+Turbo.noisy <- ApplyNoise(turbo.coded, SNR.db = 2)
+Turbo.messy <- ApplyNoise(turbo.coded, SNR.db = 0.1)
 
 # show coded != noisy
 block.coded - block.noisy
 block.coded - block.messy
 
-conv.coded
 round(conv.noisy, digits = 2)
-round(conv.messy, digits = 2)
+#round(conv.messy, digits = 2)
 
-turbo.coded
 round(turbo.noisy, digits = 2)
-round(turbo.messy, digits = 2)
+#round(turbo.messy, digits = 2)
 
 # decode noisy code
-# noisy & messy!!!
 block.noisy.decoded <- BlockDecode(block.noisy, block.encoder = block.coder)
 block.messy.decoded <- BlockDecode(block.messy, block.encoder = block.coder)
 
@@ -71,9 +73,9 @@ conv.noisy.decoded <- ConvDecodeSoft(conv.noisy, conv.encoder = conv.coder)$outp
 conv.messy.decoded <- ConvDecodeSoft(conv.messy, conv.encoder = conv.coder)$output.hard
 
 turbo.noisy.decoded <- TurboDecode(turbo.noisy, permutation.vector = perm.vector,
-                             iterations = 3, coder.info = conv.coder)$output.hard
-turbo.noisy.decoded <- TurboDecode(turbo.messy, permutation.vector = perm.vector,
-                             iterations = 3, coder.info = conv.coder)$output.hard
+                             iterations = 10, coder.info = systematic.coder)$output.hard
+turbo.messy.decoded <- TurboDecode(turbo.messy, permutation.vector = perm.vector,
+                             iterations = 10, coder.info = systematic.coder)$output.hard
 
 # message = decoded ???
 message - block.noisy.decoded
@@ -90,15 +92,19 @@ message - turbo.messy.decoded
 message = c(1, 0, 1, 0, 1)
 
 block.coded <- BlockEncode(message, block.encoder = block.coder, visualize = TRUE)
-block.decoded <- BlockDecode(block.coded, block.encoder = block.coder, visualize = TRUE)
+#block.decoded <- BlockDecode(block.coded, block.encoder = block.coder)
 
 conv.coded <- ConvEncode(message, conv.encoder = conv.coder, visualize = TRUE)
-# conv.decoded <- ConvDecodeSoft(conv.coded, conv.encoder = conv.coder)
+
+perm.vector = TurboGetPermutation(message.length = length(message),
+                                  coder.info = conv.coder,
+                                  type = "RANDOM")
 
 turbo.coded <- TurboEncode(message, permutation.vector = perm.vector,
-                           coder.info = conv.coder)
-turbo.decoded <- TurboDecode(turbo.coded, permutation.vector = perm.vector,
-                             iterations = 3, coder.info = conv.coder, visualize = TRUE)
+                           coder.info = systematic.coder)
+turbo.noisy.vis <- ApplyNoise(turbo.coded, SNR.db = 3)
+turbo.decoded <- TurboDecode(turbo.noisy.vis, permutation.vector = perm.vector,
+                             iterations = 3, coder.info = systematic.coder, visualize = TRUE)
 
 ##### simulation #####
 
